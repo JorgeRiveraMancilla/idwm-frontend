b074fce37142459850747bd5d46ddeca1d4022f8
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-09-21 20:50:45 -0300

    chore(exports): add new components exports to the barrel file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,29 @@
+// Common
 export { ProductCard } from "./common/product-card";
+export { FilterBar } from "./common/filter-bar";
+export { ProductCardSkeletonGrid } from "./common/product-card-skeleton";
+
+// Layout
 export { Navbar } from "./layout/navbar";
 export { Footer } from "./layout/footer";
+
+// ShadCN
 export { Button } from "./ui/button";
+export { Input } from "./ui/input";
+export { Label } from "./ui/label";
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
+export {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
