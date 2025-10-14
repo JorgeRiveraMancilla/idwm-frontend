db5d7b509530ff93afbeb78d5124699f89162c58
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-10-14 17:24:20 -0300

    feat(imports): add the hooks for admin requests to its barrell file

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -6,6 +6,9 @@ export {
   useVerifyEmailMutation,
 } from "./use-auth-service";
 export {
+  useCreateProductMutation,
   useGetProductDetail,
+  useGetProductsForAdmin,
   useGetProductsForCustomer,
+  useToggleProductAvailabilityMutation,
 } from "./use-product-service";
