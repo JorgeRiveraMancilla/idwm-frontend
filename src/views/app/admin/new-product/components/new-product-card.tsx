c6cfdc65231959db4cbd1f16fec5ce68780cd140
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-10-16 14:02:52 -0300

    refactor(components): delete the return button on the new product card

--- src/views/app/admin/new-product/components/new-product-card.tsx
+++ src/views/app/admin/new-product/components/new-product-card.tsx
@@ -14,29 +14,14 @@ import { NewProductForm } from "./new-product-form";
 
 export function NewProductCard() {
   return (
-    <div className="flex gap-x-3.5">
-      <div className="flex items-start">
-        <Link href="/admin/products">
-          <Button
-            variant="outline"
-            className="flex items-center space-x-2 hover:bg-gray-50 cursor-pointer"
-          >
-            <ArrowLeft className="h-4 w-4" />
-            <span>Volver</span>
-          </Button>
-        </Link>
-      </div>
-      <Card className="w-full max-w-6xl mb-6 flex flex-col">
-        <CardHeader className="flex-shrink-0">
-          <CardTitle>Nuevo Producto</CardTitle>
-          <CardDescription>
-            Ingresa los datos del nuevo producto.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="flex-1">
-          <NewProductForm />
-        </CardContent>
-      </Card>
-    </div>
+    <Card className="w-full max-w-6xl mb-6 flex flex-col">
+      <CardHeader className="flex-shrink-0">
+        <CardTitle>Nuevo Producto</CardTitle>
+        <CardDescription>Ingresa los datos del nuevo producto.</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1">
+        <NewProductForm />
+      </CardContent>
+    </Card>
   );
 }
