71597088590700bdf3a25df3d6787ce5fe112883
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-10-27 20:05:13 -0300

    feat(order): add Order and GetOrdersResponse interfaces for order management

--- src/models/responses/index.ts
+++ src/models/responses/index.ts
@@ -1,4 +1,5 @@
 export { type CartItem, type GetCartResponse } from "./cart";
+export { type GetOrdersResponse, type Order, type OrderItem } from "./order";
 export {
   type ProductDetailForCustomerResponse,
   type ProductForAdminResponse,
